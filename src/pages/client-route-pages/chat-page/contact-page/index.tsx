86bc6113e751923584c5ebcad97fab1773aa769e
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-08-22 10:21:45 +0700

    fix

--- src/pages/client-route-pages/chat-page/contact-page/index.tsx
+++ src/pages/client-route-pages/chat-page/contact-page/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState, useCallback, useMemo } from "react";
 import { Input } from "@/components/ui/input";
 import { Search, Users, Send, Inbox } from "lucide-react";
