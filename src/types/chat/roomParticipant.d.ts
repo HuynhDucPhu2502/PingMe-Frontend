732bf01afffa8aced780b2c6c6b57ec6a08e9675
Le Tran Gia Huy <tranlehuygia2210@gmail.com>
2025-10-28 08:14:19 +0700

    Chỉnh sửa lại các type dành cho userStatus

--- src/types/chat/roomParticipant.d.ts
+++ src/types/chat/roomParticipant.d.ts
@@ -1,7 +1,10 @@
+import type { UserStatus } from "../common/userStatus";
 export interface RoomParticipantResponse {
   userId: number;
   name: string;
   avatarUrl: string;
+
+  status: UserStatus;
   role: "OWNER" | "ADMIN" | "MEMBER";
   lastReadMessageId: number | null;
   lastReadAt: string | null;
--- src/types/common/userSummary.d.ts
+++ src/types/common/userSummary.d.ts
@@ -1,9 +1,12 @@
 import type { FriendshipSummary } from "../friendship";
+import type { UserStatus } from "./userStatus";
 
 export interface UserSummaryResponse {
   id: number;
   email: string;
   name: string;
   avatarUrl: string;
+
+  status: UserStatus;
   friendshipSummary: FriendshipSummary | null;
 }
